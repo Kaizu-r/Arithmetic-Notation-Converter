9d1e86bea8775767bb3d196c317569105ff9ca88
Kaizu-r <jljoloc@gmail.com>
2025-04-16 20:17:41 +0800

    ADD: Variables with coefficients (considers coefficient of 1 if variable has no coefficient e.g. x + 1)

--- convert.hpp
+++ convert.hpp
@@ -13,13 +13,15 @@ typedef enum {
     POW,
     MOD,
     LEFT_P,
-    RIGHT_p,
+    RIGHT_P,
+    VAR,
+    COEFFICIENT,
     END
 }Token;
 
 
 void removeSpace(std::string str){
-    str.erase(remove(str.begin(), str.end(), isspace), str.end());
+    str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
 }
 
 int isDigit(char c){
@@ -75,10 +77,21 @@ Token toOperator(char c){
         case '%':
             return MOD;
     }
+    return END;
 }
-void tokenize(Token *t, float* nums, std::string str){
+
+int isVar(char c){
+    if(c >= 65 && c <= 90)  //BIG LETTER
+        return 1;
+    if(c >= 97 && c <= 122) //small letter
+        return 1;
+    return 0;
+}
+void tokenize(Token *t, float* nums, float* coeff, char *var,  std::string str){
     int num_top = -1;
     int token_top = -1;
+    int var_top = -1;
+    int coeff_top = -1;
 
     int i = 0;
     while(i < str.length()){
@@ -87,22 +100,78 @@ void tokenize(Token *t, float* nums, std::string str){
             int end = i;
             while(i+1<str.length() && isDigit(str.at(i+1)))
                 end = ++i;
-            nums[++num_top] = toInt(str, begin, end); 
-            t[++token_top] = DIGIT;
+            int num = toInt(str, begin, end); 
 
             //need to add float part here or maybe on different if
+
+            if(i + 1 < str.length() && isVar(str.at(i+1))){
+                coeff[++coeff_top] = num;
+                var[++var_top] = str.at(++i);
+                t[++token_top] = COEFFICIENT;
+                t[++token_top] = VAR;
+            }
+            else
+                t[++token_top] = DIGIT;
+
+        }
+        else if(isVar(str.at(i))){
+            coeff[++coeff_top] = 1;
+            var[++var_top] = str.at(i);
+            t[++token_top] = COEFFICIENT;
+            t[++token_top] = VAR;
         }
         else if(isOperator(str.at(i)))
             t[++token_top] = toOperator(str.at(i));
         else if(str.at(i) == '(')
             t[++token_top] = LEFT_P;
         else if(str.at(i) == ')')
-            t[++token_top] = RIGHT_p;
+            t[++token_top] = RIGHT_P;
         i++;
     }
     t[++token_top] = END;
 }
 
+void printTok(Token t){
+    switch(t){
+        case DIGIT:
+            std::cout << "DIGIT";
+            break;
+        case ADD:
+            std::cout << "ADD";
+            break;
+        case SUBTRACT:
+            std::cout << "SUBTRACT";
+            break;
+        case DIVIDE:
+            std::cout << "DIVIDE";
+            break;
+        case MULTIPLY:
+            std::cout << "MULTIPLY";
+            break;
+        case POW:
+            std::cout << "POW";
+            break;
+        case MOD:
+            std::cout << "MOD";
+            break;
+        case LEFT_P:
+            std::cout << "LEFT_P";
+            break;
+        case RIGHT_P:
+            std::cout << "RIGHT_P";
+            break;
+        case VAR:
+            std::cout << "VAR";
+            break;
+        case COEFFICIENT:
+            std::cout << "COEFFICIENT";
+            break;
+        case END:
+            std::cout << "END";
+            break;
+    }
+    std::cout << std::endl;
+}
 
 
 
