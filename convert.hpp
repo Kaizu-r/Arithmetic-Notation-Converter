c61c80d39cf05ab02e20fa99c055cea99e92c402
Kaizu-r <jljoloc@gmail.com>
2025-04-17 00:52:35 +0800

    ADD: infix to postfix function using shunting yard algo

--- convert.hpp
+++ convert.hpp
@@ -18,19 +18,72 @@ int precedence(Token t){
     }
 }
 
-void inToPost(Token *t, float *nums, float *coeff, char *var, Token* queue){
-    float numStack[100];
-    float coeffStack[100];
-    char varStack[100];
+int isTokOperator(Token t){
+    switch(t){
+        case ADD:
+        case SUBTRACT:
+        case DIVIDE:
+        case MULTIPLY:
+        case POW:
+        case MOD:
+            return 1;
+    }
+    return 0;
+}
 
-    int nums_top, coeff_top, var_top;
-    nums_top = -1;
-    coeff_top = -1;
-    var_top = -1;
+void inToPost(Token *t, float *nums, float *coeff, Token* queue){
+
+    Token tokStack[100];
+
+    int negative_flag = 1;
+
+    int rear, tok_top, nums_curr, coeff_curr;
+    nums_curr = -1;
+    coeff_curr = -1;
+    tok_top = -1;
+    rear = -1;
 
     while(*t != END){
-
+        if(*t == DIGIT || *t == COEFFICIENT){
+            if(*t == DIGIT){    //push first digit
+                nums[++nums_curr]*= negative_flag; 
+                queue[++rear] = DIGIT;
+                t++;
+            }
+            else{
+                coeff[++coeff_curr] *= negative_flag;
+                queue[++rear] = COEFFICIENT;
+                queue[++rear] = VAR;
+                t+=2;
+            }
+            negative_flag = 1;
+        }
+        else if(isTokOperator(*t)){
+            if(*t == SUBTRACT && rear > -1 && *(t-1) != VAR && *(t-1) != DIGIT ){   //previous token is not a number, therefore subtract is negative sign
+                negative_flag = -1;
+            }
+            else{
+                while(tok_top > -1 && tokStack[tok_top] != LEFT_P && (precedence(tokStack[tok_top]) >= precedence(*t)) && *t != POW){
+                    queue[++rear] = tokStack[tok_top--];    //pop and push everythign to queue
+                }
+                tokStack[++tok_top] = *t;
+                t++;
+            }
+        }
+        else if(*t == LEFT_P){
+            tokStack[++tok_top] = LEFT_P;
+            t++;
+        }
+        else if(*t == RIGHT_P){
+            while(tok_top > -1 && tokStack[tok_top] != LEFT_P){
+                queue[++rear] = tokStack[tok_top--]; //pop everything to queue
+            }
+            if(tok_top != -1)
+                tok_top--;
+        }
     }
+    while(tok_top > -1)
+        queue[++rear] = tokStack[tok_top--];    //pop the rest of ir
 }
 
 
