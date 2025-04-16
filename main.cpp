1271c782ca4c50a5cbcb067036695a3f715d156b
Kaizu-r <jljoloc@gmail.com>
2025-04-16 20:27:53 +0800

    TEST: Tokenizer (works)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
     std::string str;
     std::cout << "ENTER: ";
     std::getline(std::cin, str);
-    
+
     removeSpace(str);
     Token t[100];
     float num[100];
@@ -17,5 +17,21 @@ int main(){
     int i = 0;
     while(t[i] != END)
         printTok(t[i++]);
+    
+    //reprint it
+    i = 0;
+    int j = 0, k = 0, l = 0;
+    while(t[i] != END){
+        if(t[i] == DIGIT){
+            std::cout << num[j++] << ' ';
+        }
+        else if(t[i] == COEFFICIENT)
+            std::cout << coeff[k++];
+        else if(t[i] == VAR)
+            std::cout << var[l++] << ' ';
+        else
+            printTok(t[i]);
+        i++;
+    }
     
 }
